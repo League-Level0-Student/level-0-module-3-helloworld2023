06e4078ba609637bb656b522ad4b0e6c0cd82385
blackbeltcam <league@imac.attlocal.net>
2018-09-29 11:32:39 -0700

    Mod 3

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -20,7 +20,7 @@ import javax.swing.JOptionPane;
 * 
 * Print your results to the console, or using JOptionPane if you like.
 * 
-* If your code is correct, the output will be:
+* If your code is correct, the output will be;kjl:
 *  
 * 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
 * 
